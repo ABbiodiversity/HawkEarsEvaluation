c60500c7790dd2071b5c48dc44c961b3fac83707
ecknight <ellyknight@gmail.com>
2024-03-28 16:03:55 -0600

    use interval join

--- wrangling/01_wrangling_expert.R
+++ wrangling/01_wrangling_expert.R
@@ -4,6 +4,7 @@
 
 #1. Load libraries----
 library(tidyverse)
+library(fuzzyjoin)
 
 #2. Set root file path---
 root <- "G:/Shared drives/ABMI_Recognizers/HawkEars"
@@ -74,33 +75,46 @@ raw <- do.call(rbind, list.he) %>%
   rbind(do.call(rbind, list.bn))
 
 write.csv(raw, file.path(root, "Results", "ExpertData", "ExpertData_HawkEarsBirdNET_raw.csv"), row.names = FALSE)
+
 raw <- read.csv(file.path(root, "Results", "ExpertData", "ExpertData_HawkEarsBirdNET_raw.csv"))
 
-#2. Get inventory of minutes of expert data----
-dur <- eval %>% 
-  dplyr::select(recording_id, minute) %>% 
-  unique()
+interval_full_join(dplyr::filter(eval, fileid==files[i]))
 
-#2. Summarize to minute----
-min <- raw %>% 
-  mutate(minute = ceiling(start/60),
-         minute = ifelse(minute==0, 1, minute)) %>% 
-  group_by(classifier, recording_id, minute, species) %>% 
-  summarize(score = max(score)) %>% 
-  pivot_wider(names_from=classifier, values_from=score) %>% 
-  dplyr::filter(minute <=3) %>% 
-  mutate(recording_id=as.integer(recording_id))
-
-#3. Put everything together----
+#3. Wrangle evaluation data to intervals----
 dat <- eval %>%
   dplyr::select(recording_id, minute, observer_id, ALFL:YRWA) %>% 
   pivot_longer(ALFL:YRWA, values_to="count", names_to="species") %>% 
   dplyr::filter(count>0) %>% 
   rbind(extra) %>% 
-  left_join(covs %>% 
-              dplyr::select(recording_url, recording_id, minute), multiple="all") %>% 
-  full_join(min) %>% 
-  dplyr::select(observer_id, recording_id, minute, species, count, BirdNET, HawkEars) %>% 
-  inner_join(dur)
+  mutate(start = 60*(minute-1),
+         end = 60*minute)
 
-write.csv(dat, file.path(root, "Results", "ExpertData", "ExpertData_ByMinute.csv"), row.names = FALSE)
+#4. List of species and recordings----
+loop <- dplyr::select(raw, recording_id, species) %>% 
+  unique() %>% 
+  rbind(dplyr::select(dat, recording_id, species) %>% 
+          unique()) %>% 
+  unique() %>% 
+  arrange(species, recording_id)
+
+#THERE'S GOTTA BE A FASTER WAY OF DOING THIS
+
+#5. Set up loop----
+out.list <- list()
+for(i in 1:nrow(loop)){
+  
+  #6. Interval join evaluation and raw data----
+  out.list[[i]] <- dplyr::filter(dat, recording_id==loop$recording_id[i], species==loop$species[i]) %>%
+    dplyr::select(-recording_id, -species) %>% 
+    interval_full_join(dplyr::filter(raw, recording_id==loop$recording_id[i], species==loop$species[i]) %>% 
+                         dplyr::select(-recording_id, -species)) %>% 
+    mutate(recording_id = loop$recording_id[i],
+           species = loop$species[i])
+
+print(paste0("Finished ", i, " of ", nrow(loop)))
+  
+}
+
+out <- do.call(rbind, out.list)
+
+write.csv(out, file.path(root, "Results", "ExpertData", "ExpertData_IntervalJoin.csv"), row.names = FALSE)
