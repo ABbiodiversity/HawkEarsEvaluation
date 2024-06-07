89a453139a569f6a73d6cb0aa5008130513fc102
ecknight <ellyknight@gmail.com>
2024-06-06 20:09:26 -0600

    Add perch, tidy

--- evaluation/01_evaluation_expert.R
+++ evaluation/01_evaluation_expert.R
@@ -4,10 +4,6 @@
 
 #1. Load libraries----
 library(tidyverse)
-# library(Metrics)
-# library(PRROC)
-# library(yardstick)
-# library(carat)
 
 #2. Set root file path---
 root <- "G:/Shared drives/ABMI_Recognizers/HawkEars"
@@ -15,18 +11,18 @@ root <- "G:/Shared drives/ABMI_Recognizers/HawkEars"
 #3. Load data----
 raw <- read.csv(file.path(root, "Results", "ExpertData", "ExpertData_ByMinute.csv"))
 
-#3. Set species list----
+#4. Set species list----
 sp <- raw |> 
   dplyr::filter(!is.na(HawkEars)) |> 
   dplyr::select(species) |> 
   unique()
 
-#4. Wrangle----
+#5. Wrangle----
 min <- raw |> 
   dplyr::filter(species %in% sp$species) |> 
-  mutate(actual = ifelse(is.na(count), 0, 1))
+  mutate(actual = ifelse(count==0, 0, 1))
 
-#5. Write functions----
+#6. Write functions----
 calculate_prf <- function(threshold, data, human_total){
   
   #Summarize
@@ -51,119 +47,88 @@ calculate_richness <- function(threshold, data){
   
 }
 
-#6. Set threshold sequence----
+#7. Set threshold sequence----
 threshold <- seq(0.1, 0.99, 0.01)
 
-# #4. Wrangle for yardstick----
-# min.bn <- min %>% 
-#   mutate(truth = factor(ifelse(actual==1, "Present", "Absent"))) %>% 
-#   rename(Present = BirdNET) %>% 
-#   mutate(Absent = 1 - Present) %>% 
-#   mutate(predicted = factor(ifelse(Present > Absent, "Present", "Absent"))) %>% 
-#   dplyr::select(truth, Present, Absent, predicted, species)
-# 
-# min.he <- min %>% 
-#   mutate(truth = factor(ifelse(actual==1, "Present", "Absent"))) %>% 
-#   rename(Present = HawkEars) %>% 
-#   mutate(Absent = 1 - Present) %>% 
-#   mutate(predicted = factor(ifelse(Present > Absent, "Present", "Absent"))) %>% 
-#   dplyr::select(truth, Present, Absent, predicted, species)
-# 
-# #ALL SPECIES########
-# 
-# #1. ROC AUC----
-# auc(min$actual, min$BirdNET)
-# auc(min$actual, min$HawkEars)
-# 
-# roc_auc(min.bn, truth, Present)
-# autoplot(roc_curve(min.bn, truth, Present))
-# roc_auc(min.he, truth, Present)
-# autoplot(roc_curve(min.he, truth, Present))
-# 
-# #2. MAP (PR AUC)----
-# pr_auc(min.bn, truth, Present)
-# pr_curve(min.bn, truth, Present) %>% autoplot()
-# 
-# pr_auc(min.he, truth, Present)
-# pr_curve(min.he, truth, Present) %>% autoplot()
-# 
-# pr <- pr_curve(min.bn, truth, Present) |> 
-#   mutate(thresh = round(.threshold, 2)) |> 
-#   group_by(thresh) |> 
-#   summarize(precision = mean(precision), 
-#             recall = mean(recall)) |> 
-#   ungroup()
-# 
-# pr.old <- read.csv(file.path(root, "Results", "ExpertData", "ExpertData_PR_Total.csv"))
-# 
-# pr.both <- pr |> 
-#   right_join(pr.old |> 
-#                dplyr::filter(classifier=="BirdNET"))
-# 
-# ggplot(pr.both) +
-#   geom_point(aes(x=p, y=precision)) +
-#   geom_abline(aes(intercept=0, slope=1))
-# 
-# ggplot(pr.both) +
-#   geom_point(aes(x=r, y=recall)) +
-#   geom_abline(aes(intercept=0, slope=1))
-# 
-# #3. MAPc (mean PR AUC across species)----
-# min.bn %>% 
-#   group_by(species) %>% 
-#   pr_auc(truth, Present) %>% 
-#   summarize(cmap = mean(.estimate, na.rm=TRUE))
-# 
-# min.he %>% 
-#   group_by(species) %>% 
-#   pr_auc(truth, Present) %>% 
-#   summarize(cmap = mean(.estimate, na.rm=TRUE))
-
-#COMMUNITY METRICS##############
+#OVERALL METRICS##############
 
 #1. Wrangle----
 dat.he <- min |> 
-  mutate(tp = ifelse(!is.na(HawkEars) & !is.na(count), 1, 0),
-         fp = ifelse(!is.na(HawkEars) & is.na(count), 1, 0),
-         fn = ifelse(is.na(HawkEars) & !is.na(count), 1, 0)) |> 
-  dplyr::select(-BirdNET) |> 
+  mutate(tp = ifelse(!is.na(HawkEars) & count > 0, 1, 0),
+         fp = ifelse(!is.na(HawkEars) & count == 0, 1, 0),
+         fn = ifelse(is.na(HawkEars) & count > 0, 1, 0)) |> 
+  dplyr::select(-BirdNET, -Perch) |> 
   rename(confidence = HawkEars)
 
 dat.bn <- min |> 
-  mutate(tp = ifelse(!is.na(BirdNET) & !is.na(count), 1, 0),
-         fp = ifelse(!is.na(BirdNET) & is.na(count), 1, 0),
-         fn = ifelse(is.na(BirdNET) & !is.na(count), 1, 0)) |> 
-  dplyr::select(-HawkEars) |> 
+  mutate(tp = ifelse(!is.na(BirdNET) & count > 0, 1, 0),
+         fp = ifelse(!is.na(BirdNET) & count == 0, 1, 0),
+         fn = ifelse(is.na(BirdNET) & count > 0, 1, 0)) |> 
+  dplyr::select(-HawkEars, -Perch) |> 
   rename(confidence = BirdNET)
 
-#2. Set up recording loop----
+dat.pr <- min |> 
+  mutate(tp = ifelse(!is.na(Perch) & count > 0, 1, 0),
+         fp = ifelse(!is.na(Perch) & count == 0, 1, 0),
+         fn = ifelse(is.na(Perch) & count > 0, 1, 0)) |> 
+  dplyr::select(-HawkEars, -BirdNET) |> 
+  rename(confidence = Perch)
+
+#2. Total detections----
+human_total <- sum(min$actual)
+
+#3. Overall metrics----
+overall.he <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.he, human_total = human_total)) |> 
+  mutate(classifier="HawkEars")
+overall.bn <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.bn, human_total = human_total)) |> 
+  mutate(classifier = "BirdNET")
+overall.pr <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.pr, human_total = human_total)) |> 
+  mutate(classifier = "Perch")
+
+overall <- rbind(overall.he, overall.bn, overall.pr) |> 
+  pivot_longer(precision:fscore, names_to="metric", values_to="value")
+
+#4. Plot----
+ggplot(overall) +
+  geom_line(aes(x=threshold, y=value, colour=classifier)) +
+  facet_wrap(~metric, scales="free")
+
+#PER RECORDING#######
+
+#1. Set up recording loop----
 recs <- unique(dat.he$recording_id)
 
 rec.list <- list()
 for(i in 1:length(recs)){
   
-  #3. Subset data----
+  #2. Subset data----
   dat.he.i <- dat.he |> 
     dplyr::filter(recording_id==recs[i])
   dat.bn.i <- dat.bn |> 
     dplyr::filter(recording_id==recs[i])
+  dat.pr.i <- dat.pr |> 
+    dplyr::filter(recording_id==recs[i])
   
-  #4. Get total expert detections----
+  #3. Get total expert detections----
   human_total = sum(dat.he.i$actual)
   
-  #3. Get PRF----
+  #4. Get PRF----
   prf.he <-  do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.he.i, human_total=human_total))
   prf.bn <-  do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.bn.i, human_total=human_total))
+  prf.pr <-  do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.pr.i, human_total=human_total))
   
-  #4. Get species richness----
+  #5. Get species richness----
   rich.he <- do.call(rbind, lapply(X=threshold, FUN=calculate_richness, data=dat.he.i))
   rich.bn <- do.call(rbind, lapply(X=threshold, FUN=calculate_richness, data=dat.bn.i))
+  rich.pr <- do.call(rbind, lapply(X=threshold, FUN=calculate_richness, data=dat.pr.i))
   
-  #5. Wrangle----
+  #6. Wrangle----
   rec.list[[i]] <- inner_join(prf.he, rich.he) |> 
     mutate(classifier = "HawkEars") |> 
     rbind(inner_join(prf.bn, rich.bn) |> 
             mutate(classifier = "BirdNET")) |> 
+    rbind(inner_join(prf.pr, rich.pr) |> 
+            mutate(classifier = "Perch")) |> 
     pivot_longer(c(precision, recall, fscore, richness), names_to="metric", values_to="value") |> 
     mutate(recording_id = recs[i])
   
@@ -171,29 +136,25 @@ for(i in 1:length(recs)){
   
 }
 
+#7. Put together----
 out <- do.call(rbind, rec.list)
 
+#8. Summarize----
 out.rec <- out |> 
   group_by(threshold, classifier, metric) |> 
   summarize(mn = mean(value, na.rm = TRUE),
             std = sd(value, na.rm = TRUE)) |> 
   ungroup()
 
-out.sd <- out |> 
-  group_by(threshold, classifier, metric) |> 
-  summarize(std = sd(value, na.rm=TRUE)) |> 
-  ungroup()
-
-#6. Plot----
+#9. Plot----
 ggplot(out.rec) + 
   geom_ribbon(aes(x=threshold, ymin = mn-std, ymax = mn+std, group=classifier), alpha = 0.3) +
   geom_line(aes(x=threshold, y=mn, colour=classifier)) + 
   facet_wrap(~metric, scales="free")
 
-#7. Save-----
-write.csv(out, file.path(root, "Results", "ExpertData", "PRFRichness_Overall.csv"), row.names = FALSE)
+#10. Save-----
+write.csv(out, file.path(root, "Results", "ExpertData", "PRFRichness_Summary.csv"), row.names = FALSE)
 write.csv(out.rec, file.path(root, "Results", "ExpertData", "PRFRichness_Recording.csv"), row.names = FALSE)
-
 
 #PER SPECIES#########
 
@@ -205,26 +166,33 @@ for(i in 1:nrow(sp)){
   #2. Wrangle----
   dat.he <- min |> 
     dplyr::filter(species==sp$species[i]) |> 
-    mutate(tp = ifelse(!is.na(HawkEars) & !is.na(count), 1, 0),
-           fp = ifelse(!is.na(HawkEars) & is.na(count), 1, 0),
-           fn = ifelse(is.na(HawkEars) & !is.na(count), 1, 0)) |> 
-    dplyr::select(-BirdNET) |> 
+    mutate(tp = ifelse(!is.na(HawkEars) & count > 0, 1, 0),
+           fp = ifelse(!is.na(HawkEars) & count == 0, 1, 0),
+           fn = ifelse(is.na(HawkEars) & count > 0, 1, 0)) |> 
+    dplyr::select(-BirdNET, -Perch) |> 
     rename(confidence = HawkEars)
   
   dat.bn <- min |> 
     dplyr::filter(species==sp$species[i]) |> 
-    mutate(tp = ifelse(!is.na(BirdNET) & !is.na(count), 1, 0),
-           fp = ifelse(!is.na(BirdNET) & is.na(count), 1, 0),
-           fn = ifelse(is.na(BirdNET) & !is.na(count), 1, 0)) |> 
-    dplyr::select(-HawkEars) |> 
+    mutate(tp = ifelse(!is.na(BirdNET) & count > 0, 1, 0),
+           fp = ifelse(!is.na(BirdNET) & count == 0, 1, 0),
+           fn = ifelse(is.na(BirdNET) & count > 0, 1, 0)) |> 
+    dplyr::select(-HawkEars, -Perch) |> 
     rename(confidence = BirdNET)
+  
+  dat.pr <- min |> 
+    dplyr::filter(species==sp$species[i]) |> 
+    mutate(tp = ifelse(!is.na(Perch) & count > 0, 1, 0),
+           fp = ifelse(!is.na(Perch) & count == 0, 1, 0),
+           fn = ifelse(is.na(Perch) & count > 0, 1, 0)) |> 
+    dplyr::select(-HawkEars, -BirdNET) |> 
+    rename(confidence = Perch)
   
   #3. Get total expert detections----
   human_total = sum(dat.he$actual)
   
   #4. Apply function----
   prf <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.he, human_total=human_total)) 
-  
   #5. Get F-score threshold----
   fscore <- arrange(prf, -fscore)[1,] |> 
     mutate(thresh = "fscore")
@@ -240,7 +208,7 @@ for(i in 1:nrow(sp)){
     unique() |> 
     summarize(recordings = n())
   
-  #7. Put together----
+  #8. Put together----
   out.list[[i]] <- rbind(fscore, universe) |> 
     dplyr::select(-fscore) |> 
     pivot_wider(names_from=thresh, values_from=precision:threshold) |> 
@@ -250,30 +218,35 @@ for(i in 1:nrow(sp)){
            hits = human_total) |> 
     dplyr::select(species, recordings, hits, precision_universal, recall_universal, threshold_fscore, precision_fscore, recall_fscore)
   
-  #8. Apply function to birdnet----
+  #9. Apply function to birdnet----
   prf.bn <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.bn, human_total=human_total))
   
-  #9. Put together----
+  #10. Apply function to birdnet----
+  prf.pr <- do.call(rbind, lapply(X=threshold, FUN=calculate_prf, data=dat.pr, human_total=human_total))
+  
+  #11. Put together----
   prf.list[[i]] <- prf |> 
     mutate(classifier="HawkEars") |> 
     rbind(prf.bn |> 
             mutate(classifier="BirdNET")) |> 
+    rbind(prf.pr |> 
+            mutate(classifier="Perch")) |> 
     mutate(species = sp$species[i])
   
   cat("Finished species", sp$species[i], ":", i, "of", nrow(sp), "\n")
   
 }
 
-#8. Save----
+#12. Save----
 out.sp <- do.call(rbind, out.list) |> 
   arrange(species)
 
 prf <- do.call(rbind, prf.list)
 
-write.csv(out.sp, file.path(root, "Results", "ExpertData", "PRF.csv"), row.names = FALSE)
+write.csv(out.sp, file.path(root, "Results", "ExpertData", "PRF_Species_Summary.csv"), row.names = FALSE)
 write.csv(prf, file.path(root, "Results", "ExpertData", "PRF_Species.csv"), row.names = FALSE)
 
-#9. Interrogate----
+#13. Interrogate----
 prf <- read.csv(file.path(root, "Results", "ExpertData", "PRF_Species.csv"))
 prf.maxf <- prf |> 
   group_by(species, classifier) |> 
